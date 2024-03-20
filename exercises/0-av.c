b46d2d70ac9445f89bda8ce8bf65338cd2605282
“kelciatkinson” <kelciatkinson@gmail.com>
2024-03-20 20:53:53 +0000

    pid.c

--- exercises/0-av.c
+++ exercises/0-av.c
@@ -1,3 +1,17 @@
 /**
  * Write a program that prints all the arguments, without using ac.
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	while (*argv)
+	{
+		printf("%s\n",  *argv);
+		argv++;
+	}
+	printf("%u\n", argc);
+	return (0);
+}
