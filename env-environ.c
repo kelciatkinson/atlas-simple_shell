7844585d2b5560114bdf95b3d242e5201f6db838
kelciatkinson <106333133+kelciatkinson@users.noreply.github.com>
2024-03-23 20:10:27 -0500

    Update env-environ.c

---
+++ env-environ.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern char **environ
+char **add_env(const char *key_value)
+{
+  int i = 0;
+  int n = 0;
+
+  while (environ[i])
+    i++;
+
+  char **new_env = malloc((i +2) * sizeof(char *));
+
+  if (new_env == NULL)
+  {
+    perror("Memory allocation failed");
+    exit(EXIT_FAILURE);
+  }
+  
+}
