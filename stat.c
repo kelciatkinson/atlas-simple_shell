1742fd4c9424b69a1a7a9f8b228539c56adbc07e
“kelciatkinson” <kelciatkinson@gmail.com>
2024-03-26 16:48:37 +0000

    fixed betty spacing

--- stat.c
+++ stat.c
@@ -4,34 +4,35 @@
 #include <unistd.h>
 
 /**
- * __main - stat example
- * @ac: arg count
- * @av: argv pointer to an array of pointers to strings
- * Return: Always 0.
- */
+* __main - stat example
+* @ac: arg count
+* @av: argv pointer to an array of pointers to strings
+* Return: Always 0.
+*/
 int __main(int ac, char **av)
 {
-    unsigned int i;
-    struct stat st;
+	unsigned int i;
 
-    if (ac < 2)
-    {
-        printf("Usage: %s path_to_file ...\n", av[0]);
-        return (1);
-    }
-    i = 1;
-    while (av[i])
-    {
-        printf("%s:", av[i]);
-        if (stat(av[i], &st) == 0)
-        {
-            printf(" FOUND\n");
-        }
-        else
-        {
-            printf(" NOT FOUND\n");
-        }
-        i++;
-    }
-    return (0);
+	struct stat st;
+
+	if (ac < 2)
+	{
+		printf("Usage: %s path_to_file ...\n", av[0]);
+		return (1);
+	}
+	i = 1;
+	while (av[i])
+	{
+		printf("%s:", av[i]);
+		if (stat(av[i], &st) == 0)
+		{
+			printf(" FOUND\n");
+		}
+		else
+		{
+			printf(" NOT FOUND\n");
+		}
+		i++;
+	}
+	return (0);
 }
--- wait.c
+++ wait.c
@@ -4,30 +4,30 @@
 #include <sys/wait.h>
 
 /**
- * ___main - fork & wait example
- *
- * Return: Always 0.
- */
+* ___main - fork & wait example
+*
+* Return: Always 0.
+*/
 int ___main(void)
 {
-    pid_t child_pid;
-    int status;
+	pid_t child_pid;
+	int status;
 
-    child_pid = fork();
-    if (child_pid == -1)
-    {
-        perror("Error:");
-        return (1);
-    }
-    if (child_pid == 0)
-    {
-        printf("Wait for me, wait for me\n");
-        sleep(3);
-    }
-    else
-    {
-        wait(&status);
-        printf("Oh, it's all better now\n");
-    }
-    return (0);
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("Error:");
+		return (1);
+	}
+	if (child_pid == 0)
+	{
+		printf("Wait for me, wait for me\n");
+		sleep(3);
+	}
+	else
+	{
+		wait(&status);
+		printf("Oh, it's all better now\n");
+	}
+	return (0);
 }
