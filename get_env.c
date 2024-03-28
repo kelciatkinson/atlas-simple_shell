d56c823e490022c04009cb73a1b7e9e62ab7af1c
jerm014 <jeremy.mitts@atlasschool.com>
2024-03-28 22:54:55 +0000

    corrected space issue for betty

--- get_env.c
+++ get_env.c
@@ -1,21 +1,20 @@
 #include "shell.h"
 
-
 char *get_env(const char *key_value, char **env)
 {
-  size_t len = 0;
-  int i = 0;
+	size_t len = 0;
+	int i = 0;
 
-  len = strlen(key_value);
+	len = strlen(key_value);
 
-  while (env[i])
-  {
-	if (strncmp(key_value, env[i], len) == 0)
+	while (env[i])
 	{
+		if (strncmp(key_value, env[i], len) == 0)
+		{
 		if (env[i][len] == '=')
 			return (env[i] + len + 1);
+		}
+		i++;
 	}
-	i++;
-  }
 	return (NULL);
 }
