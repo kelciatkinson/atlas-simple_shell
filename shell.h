968beb424c67b6ddd4839012b9f5224c4957fc93
“kelciatkinson” <kelciatkinson@gmail.com>
2024-03-28 18:21:34 +0000

    included headers for stat in shell.h

--- shell.h
+++ shell.h
@@ -4,7 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-#include <sys/>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <string.h>
 #include <stddef.h>
