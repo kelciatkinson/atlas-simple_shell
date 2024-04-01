ecca536747fa07e948fe7f8d8a63b5b2e887762a
kelciatkinson <106333133+kelciatkinson@users.noreply.github.com>
2024-04-01 15:56:08 -0500

    update main to continue after calling printenv in interactive mode

--- main.c
+++ main.c
@@ -41,7 +41,7 @@ int main(int argc, char **argv, char **env)
 				free(buffer);
 				buffer = NULL;
 				_printenv(env);
-				exit(EXIT_SUCCESS);
+				continue;
 			}
 			tokens = tokenize(buffer, " \n");
 			found = findpath(tokens[0], env);
