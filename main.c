f2e5a92b48fdfbf6430397285b2bd5d53ce7b60f
jerm014 <jeremy.mitts@atlasschool.com>
2024-04-01 22:24:57 +0000

    corrected another memory leak

--- main.c
+++ main.c
@@ -34,8 +34,7 @@ int main(int argc, char **argv, char **env)
 			{
 				if (buffer[4] != '\0')
 				{
-					buffer += 4;
-					exit_val = atoi(buffer);
+					exit_val = atoi(buffer + 4);
 					free(buffer);
 					exit(exit_val);
 				}
